e8353fc7067e29cc64cb0d37aee61b5d2d2529f6
Dan LaBruna <DanielSLaBruna@gmail.com>
2019-05-06 18:42:24 -0400

    Initial commit

---
+++ dataset.py
@@ -0,0 +1,103 @@
+"""
+
+Initial dataset preprocessing.
+
+"""
+
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+tqdm.pandas()
+
+# Tables.
+AGE_TABLE = {
+    "0-17": 0,
+    "18-25": 1,
+    "26-35": 2,
+    "36-45": 3,
+    "46-50": 4,
+    "51-55": 5,
+    "55+": 6,
+    np.NAN: -1
+}
+
+GENDER_TABLE = {
+    'F': 0,
+    'M': 1,
+    np.NAN: -1
+}
+
+CITY_TABLE = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    np.NAN: -1
+}
+
+
+def clean_data(row_data):
+    # Gender.
+    gender = GENDER_TABLE[row_data['Gender']]
+
+    # Age.
+    age = AGE_TABLE[row_data['Age']]
+
+    # City category.
+    city_category = CITY_TABLE[row_data['City_Category']]
+
+
+    # City stay.
+    city_stay = row_data['Stay_In_Current_City_Years']
+    if city_stay == '4+':
+        city_stay = 4
+
+    return [gender, age, city_category, city_stay]
+
+
+
+
+"""
+Data imputation methods.
+"""
+def impute_missing_vals(method, row_data):
+    # Try several methods for imputation.
+
+    # Method 0: Replace Nans w/ 0s.
+    if method == 0:
+        row_data.fillna(-1)
+
+    # Method 1: Predict using Network.
+    elif method == 1:
+        assert 0
+
+    # Method 2: Predict using KNN.
+    else:
+        assert 0
+
+    return row_data[['Product_Category_1', 'Product_Category_2', 'Product_Category_3']]
+
+
+
+
+
+
+def load_data():
+    # Import data.
+    df = pd.read_csv("data/BlackFriday.csv", nrows=10000)
+
+    df[['Gender', 'Age', 'City_Category', 'Stay_In_Current_City_Years']] = df.iloc[:, :].progress_apply(lambda x: pd.Series(clean_data(x)), axis=1)
+    print("Gender, Age, and Current Stay Corrected!")
+
+    df[['Product_Category_1', 'Product_Category_2', 'Product_Category_3']] = df.iloc[:, :].progress_apply(lambda x: pd.Series(impute_missing_vals(0, x)), axis=1)
+    print("Missing Values Imputed!")
+
+    df = df.drop(columns=['Product_ID'])
+
+    df[['Gender', 'Age', 'Occupation', 'City_Category', 'Stay_In_Current_City_Years', 'Marital_Status']] = df[['Gender', 'Age', 'Occupation', 'City_Category', 'Stay_In_Current_City_Years', 'Marital_Status']].apply(pd.to_numeric, errors='coerce')
+
+
+
+
+    print(df.dtypes)
+
+    return df
---
+++ regressions.py
@@ -0,0 +1,62 @@
+"""
+
+Initial Regressions and Visualizations.
+
+"""
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib as mpl
+mpl.style.use('seaborn')
+
+from dataset import load_data
+
+
+def select_features(df):
+
+    # 70/30 train/test.
+    X_train, X_test = train_test_split(df.iloc[:, ['Gender', 'Age', 'Occupation', 'City_Category', 'Marital_Status']], test_size=.3)
+    y_train, y_test = train_test_split(df.iloc[:, 'Purchase'], test_size=.3)
+
+    return X_train, X_test, y_train, y_test
+
+
+def plot_results(y_test, y_predicted):
+
+    print(f'Avg. distance between predictions and real values: {np.mean(np.abs(y_test - y_predicted))}')
+
+    plt.plot(range(y_test.shape[0]), y_test, label="Real Purchase Amount")
+    plt.plot(range(y_predicted.shape[0]), y_predicted, label="Predicted Purchase Amount")
+
+    plt.xlabel("Customer")
+    plt.ylabel("Purchase Amount")
+    plt.title("Linear Regression Results on Gender, Age, Occup., City Cat., M. Status")
+    plt.legend(loc='best')
+    plt.show()
+
+
+
+def poly_regression(df, degree):
+
+    X_train, X_test, y_train, y_test = select_features(df)
+
+    poly = PolynomialFeatures(degree=degree)
+    X_train_poly = poly.fit_transform(X_train)
+    X_test_poly = poly.fit_transform(X_test)
+
+    regress = LinearRegression().fit(X_train_poly, y_train)
+    y_predicted = regress.predict(X_test_poly)
+
+    print('Coefficients:')
+    print(regress.coef_)
+
+    plot_results(y_test, y_predicted)
+
+
+if __name__ == '__main__':
+    poly_regression(load_data(), 1)
+
